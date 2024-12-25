471a0188dc82fe78f717d4ac13de48410791d033
HarishSankarK <harishsankar133@gmail.com>
2024-12-25 11:21:23 +0530

    Initial commit

--- pf.js
+++ pf.js
@@ -15,3 +15,20 @@ function doPost(e) {
   MailApp.sendEmail(recipient, subject, body);
   return ContentService.createTextOutput("Message Sent Successfully!");
 }
+function doPost(e) {
+  var name = e.parameter.name;
+  var email = e.parameter.email;
+  var subject = e.parameter.subject;
+  var message = e.parameter.message;
+
+  var recipient = "harishsankar133@gmail.com";
+  var subject = "New Contact Form Submission: " + subject;
+  var body = "You have received a new message from the contact form on your website.\n\n" +
+             "Name: " + name + "\n" +
+             "Email: " + email + "\n" +
+             "Subject: " + subject + "\n" +
+             "Message: " + message;
+
+  MailApp.sendEmail(recipient, subject, body);
+  return ContentService.createTextOutput("Message Sent Successfully!");
+}
