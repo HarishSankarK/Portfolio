175fe873c4f7dd0a8d46f92c225b69dfae9885c6
Harish Sankar K <harishsankar133@gmail.com>
2025-03-26 21:51:48 +0530

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,66 +1,34 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form data
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+    // Get form data and sanitize it
+    $name = htmlspecialchars($_POST['name']);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
 
     // Recipient email
-    $to = "harishsankar133@gmail.com";
+    $to = "harishsankar133@gmail.com"; // Change this if needed
 
-    // Email subject
-    $email_subject = "New Contact Form Submission: $subject";
-
-    // Email body
-    $email_body = "
-    You have received a new message from the contact form on your website.\n\n
-    Name: $name\n
-    Email: $email\n
-    Subject: $subject\n
-    Message: $message
-    ";
-
-    // Email headers
-    $headers = "From: $email\n";
-    $headers .= "Reply-To: $email\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\n";
-
-    // Send the email
-    if (mail($to, $email_subject, $email_body, $headers)) {
-        echo "<p>Message sent successfully!</p>";
-    } else {
-        echo "<p>Error sending message. Please try again later.</p>";
+    // Validate email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "<p>Invalid email format. Please enter a valid email.</p>";
+        exit;
     }
-}
-?>
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form data
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-
-    // Recipient email
-    $to = "harishsankar133@gmail.com";
 
     // Email subject
     $email_subject = "New Contact Form Submission: $subject";
 
     // Email body
-    $email_body = "
-    You have received a new message from the contact form on your website.\n\n
-    Name: $name\n
-    Email: $email\n
-    Subject: $subject\n
-    Message: $message
-    ";
+    $email_body = "You have received a new message from your website contact form.\n\n";
+    $email_body .= "Name: $name\n";
+    $email_body .= "Email: $email\n";
+    $email_body .= "Subject: $subject\n";
+    $email_body .= "Message:\n$message\n";
 
     // Email headers
-    $headers = "From: $email\n";
-    $headers .= "Reply-To: $email\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\n";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
     // Send the email
     if (mail($to, $email_subject, $email_body, $headers)) {
